0c0930c52de41e135cc119e913ec999b9ddf7b13
Jonah Butler <jonahbutler6@gmail.com>
2026-08-30 11:49:57 -0400

    copy pasta fix

--- repository/users/authenticate_user.sql
+++ repository/users/authenticate_user.sql
@@ -3,11 +3,11 @@ SELECT
   ,u.username
   ,u.email
   ,u.password
+  ,EXISTS (
+    SELECT 1
+    FROM user_tokens ut
+    WHERE ut.user_id = u.user_id
+      AND ut.type = 1
+  ) AS account_pending
 FROM users u
-EXISTS (
-  SELECT 1
-  FROM user_tokens ut
-  WHERE ut.user_id = u.user_id
-    AND ut.type = 1
-) AS account_pending
-WHERE email = $1;
+WHERE u.email = $1;
